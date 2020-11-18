94e9bfbf87c896cdd66162ddadad78d478c3f293
Alexandre <Alexandre.chekroun@etu.univ-paris1.fr>
2020-11-18 21:09:02 +0100

    test

--- project-jee8-model/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/jaxb/LocalDateTimeAdapter.java
+++ project-jee8-model/src/main/java/fr/pantheonsorbonne/ufr27/miage/model/jaxb/LocalDateTimeAdapter.java
@@ -9,6 +9,7 @@ public class LocalDateTimeAdapter extends XmlAdapter<String, LocalDateTime> {
 
 	private DateTimeFormatter dateFormat = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
 
+	// TEST
 	@Override
 	public String marshal(LocalDateTime dateTime) {
 		return dateTime.format(dateFormat);
