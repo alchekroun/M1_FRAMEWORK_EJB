50c5ad4edee88ef3253b7236cd53bfee9c125fff
Nicolas Herbaut <nicolas.herbaut@univ-paris1.fr>
2021-01-04 09:19:22 +0100

    fixed build (didn't compile for me)

--- project-jee8-webapp/src/test/java/fr/pantheonsorbonne/ufr27/miage/dao/AllTestsDAO.java
+++ project-jee8-webapp/src/test/java/fr/pantheonsorbonne/ufr27/miage/dao/AllTestsDAO.java
@@ -1,15 +1,8 @@
 package fr.pantheonsorbonne.ufr27.miage.dao;
 
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({ TestArretDAO.class, 
-		TestHeureDePassageDAO.class, 
-		TestInfoGareDAO.class, 
-		TestPassagerDAO.class,
-		TestTrainDAO.class })
+
+
 public class AllTestsDAO {
 
 }
